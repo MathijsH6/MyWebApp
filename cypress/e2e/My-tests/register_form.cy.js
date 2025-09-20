853b74cf92e2fa85e5134ab314f7357f270815bb
MathijsH6 <mathijs.haest@ziggo.nl>
2025-09-20 15:56:21 +0200

    laatste commit

--- cypress/e2e/My-tests/register_form.cy.js
+++ cypress/e2e/My-tests/register_form.cy.js
@@ -1,6 +1,24 @@
-describe('Register Form Validatie', () => {
+describe('Register Form', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/register');
+  });
+
+  it('registreert een nieuw account succesvol', () => {
+    const randomEmail = `testuser_${Date.now()}@sakila.test`;
+
+    cy.get('#firstname').type('Test');
+    cy.get('#lastname').type('Gebruiker');
+    cy.get('#email').type(randomEmail);
+    cy.get('#password').type('Test123!');
+    cy.get('#address1').type('Teststraat 1');
+    cy.get('#address2').type('Apt 2');
+    cy.get('#postcode').type('1234AB');
+    cy.get('#city').type('Amsterdam');
+    cy.get('#district').type('Centrum');
+    cy.get('#phone').type('0612345678');
+    cy.get('#country').type('Nederland');
+
+    cy.get('form').submit();
   });
 
   it('toont validatie-meldingen bij lege verplichte velden', () => {
